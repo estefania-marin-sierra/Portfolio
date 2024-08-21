68308d92aabf04c1e3f48cf3e32ae2f4c7f38824
estefania marin sierra <emarinsierra2@gmail.com>
2024-08-21 11:03:35 -0400

    cleanly fixed the video and spacing

--- src/app/dialog-detail/dialog-detail.component.ts
+++ src/app/dialog-detail/dialog-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Inject, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { AppComponent } from '../app.component';
@@ -11,37 +11,11 @@ import { DialogData } from '../projects';
 })
 
 export class DialogDetailComponent extends AppComponent {
-  @ViewChild('videoIframe', { static: false }) iframe!: ElementRef;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    private renderer: Renderer2,
     public override _ActivatedRoute: ActivatedRoute
   ) {
     super(_ActivatedRoute);
-  }
-
-  ngAfterViewInit() {
-    const iframe = this.iframe.nativeElement;
-    
-    iframe.onload = () => {
-      try {
-        const iframeDocument = iframe.contentDocument || iframe.contentWindow?.document;
-        
-        if (!iframeDocument) {
-          console.error('Unable to access iframe document.');
-          return;
-        }
-
-        const videoElement = iframeDocument.querySelector('video');
-        
-        if (videoElement) {
-          this.renderer.setStyle(videoElement, 'width', '100%');
-          this.renderer.setStyle(videoElement, 'object-fit', 'fill');
-        }
-      } catch (error) {
-        console.error('Error accessing iframe content:', error);
-      }
-    };
   }
 }
